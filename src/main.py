dd11e6341e8d770c17b59398081cbb8b5fbfc8eb
JDWalker123 <jdwalk3r@icloud.com>
2026-03-27 17:10:39 -0400

    Added the comments

--- src/main.py
+++ src/main.py
@@ -57,6 +57,10 @@ player2_laser_cooldown = 0
 # money
 players_money = 0
 
+#these hold the last position of the ball for every frame to know which way it came from
+prev_ball_x =  game_ball.x_pos
+prev_ball_y = game_ball.y_pos
+
 ##################################################################################################
 #brick handling
 
@@ -96,17 +100,17 @@ upgrades = {
 upgrade_buttons = []
 
 ##################################################################################################
-
+#draws the upgrade panel UI
 def draw_upgrade_panel(surface, money, upgrades, font):
     panel_x = WINDOW_WIDTH - 195
     button_height = 60
     padding = 10
     small_font = pygame.font.SysFont(None, 25)
     title_font = pygame.font.SysFont(None, 35)
-
+#blits and creates the title 
     title = title_font.render("UPGRADES", True, "Cyan") #creates and blits the title
     surface.blit(title, (panel_x + 10, 10))
-
+#an array storing th ebuttons
     buttons = []
     for i, (key, upg) in enumerate(upgrades.items()) :
         y = 80 + i * (button_height + padding) #first button 80, each button is scaled downward
@@ -119,19 +123,19 @@ def draw_upgrade_panel(surface, money, upgrades, font):
             color = (30, 80, 30)
         else:
             color = (80, 30, 30)
-        
+        #draws the buttons
         pygame.draw.rect(surface, color, rect, border_radius=6)
         pygame.draw.rect(surface, "cyan", rect, 1, border_radius=6)
-
+        #draws the labels on the buttons
         label = small_font.render(upg["label"], True, "White")
         surface.blit(label, (rect.x + 8, rect.y + 8))
-
+        #draws the MAXED on the screen
         if upg["level"] >= upg["max"]:
             sub = small_font.render("MAXED", True, "Yellow")
         else: 
             sub = small_font.render(f"Cost: ${upg['cost']}", True, "Lightgray")
         surface.blit(sub, (rect.x +8, rect.y + 32))
-
+        #appends the upgrades
         buttons.append((rect,key))
     
     return buttons
@@ -139,7 +143,7 @@ def draw_upgrade_panel(surface, money, upgrades, font):
 
 
 ##################################################################################################
-
+#draws the intro screen
 def draw_intro(surface):
     big_font = pygame.font.SysFont(None, 150)
     med_font = pygame.font.SysFont(None, 50)
@@ -158,7 +162,7 @@ def draw_intro(surface):
         (WINDOW_WIDTH // 2 - subtitle_text.get_width() // 2,
          WINDOW_HEIGHT // 2),
     )
-
+    #creates a loop that flashes text
     if pygame.time.get_ticks() % 1000 < 500:
         subsubtitle_text = small_font.render("Press any key to start", True, "white")
         surface.blit(
@@ -167,11 +171,10 @@ def draw_intro(surface):
              WINDOW_HEIGHT // 2 + 150),
         )
 
-prev_ball_x =  game_ball.x_pos
-prev_ball_y = game_ball.y_pos
-
 ##################################################################################################
+#resolves the ball and rectangle collisions
 def resolve_ball_to_rect_collision(ball, prev_x, prev_y, collision_rect):
+    #creates the hitbox for the balls
     ball_rect = pygame.Rect(
             ball.x_pos - ball.radius,
             ball.y_pos - ball.radius,
@@ -180,7 +183,7 @@ def resolve_ball_to_rect_collision(ball, prev_x, prev_y, collision_rect):
         )
     if not collision_rect.colliderect(ball_rect):
         return
-    
+    #this is the prev rectangle hitbox
     prev_rect = pygame.Rect(
             prev_x - ball.radius,
             prev_y - ball.radius,
@@ -188,6 +191,7 @@ def resolve_ball_to_rect_collision(ball, prev_x, prev_y, collision_rect):
             ball.radius * 2,
         )
     
+    #determins if the ball came from these directions, and allows us to bounce of that side of the rectangle or paddle
     was_left = prev_rect.right <= collision_rect.left
     was_right = prev_rect.left >= collision_rect.right
     was_above= prev_rect.bottom <= collision_rect.top
@@ -211,10 +215,13 @@ def resolve_ball_to_rect_collision(ball, prev_x, prev_y, collision_rect):
         else:
             ball.y_pos = collision_rect.bottom + ball.radius
         ball.y_vel *= -1
+
 ##################################################################################################
+#this builds each level, 
 def build_level(level_index):
     global bricks, level_number
     bricks = []
+    #error checking for the level indexing
     try:
         layout = LEVELS[level_index %  len(LEVELS)]
     except IndexError:
@@ -226,7 +233,7 @@ def build_level(level_index):
 
     level_number = level_index + 1
     health_scale = 1 + (2 * level_number)
-
+    #this goes through each row and column and adds a brick there depending on if there is a 0 or a 1 
     for row_i, row in enumerate(layout):
         for col_i, ch in enumerate(row):
             if ch != "0":
@@ -238,7 +245,7 @@ def build_level(level_index):
                 bricks.append(brick)
 
 ##################################################################################################
-
+#this draws the win screen and displays who won
 def draw_win_screen(surface, score1, score2):
     big_font = pygame.font.SysFont(None, 150)
     med_font = pygame.font.SysFont(None, 50)
@@ -257,7 +264,7 @@ def draw_win_screen(surface, score1, score2):
     surface.blit(p2, (WINDOW_WIDTH // 2 - p2.get_width() // 2, 420))
 
 ##################################################################################################
-
+#this applies the upgrades to the level and game loop
 def apply_upgrades(key, level):
     global game_ball
     #both are handled below
@@ -265,7 +272,7 @@ def apply_upgrades(key, level):
         pass
     elif key == "laser_damage":
         pass
-    elif key == "multi_ball":
+    elif key == "multi_ball": #spawns in new balls
         from objects import Ball
         new_ball = Ball(10, game_ball.x_pos-10, game_ball.y_pos+10, -game_ball.x_vel, -game_ball.y_vel)
         extra_balls.append(new_ball)
@@ -275,13 +282,14 @@ def apply_upgrades(key, level):
 build_level(level_index)
 # main game loop
 while running:
+    #implementation to quit the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+            #checks to see if the user presses a button, and if so it starts the game
         if game_state == "intro" and event.type == pygame.KEYDOWN:
             game_state = "playing"
-
+        #This checks to see if a user buys a upgrade
         if event.type == pygame.MOUSEBUTTONDOWN and event.button ==  1:
             for rect, key in upgrade_buttons:
                 if rect.collidepoint(event.pos):
@@ -294,13 +302,13 @@ while running:
 
     # background
     display_surface.fill((10, 15, 40))
-
+    #This displays if the game state is in the intro
     if game_state == "intro":
         draw_intro(display_surface)
-
+    #handles if the game state is "Win"
     elif game_state == "win":
         draw_win_screen(display_surface, player1_score, player2_score)
-
+    #the rest of the code is for the playing game state
     elif game_state == "playing":
         # UI
         p1_text = font.render("Money:", True, "white")
@@ -320,6 +328,7 @@ while running:
         level_text = font.render(f"LEVEL: {level_number}", True, "White")
         display_surface.blit(level_text, (20, 20))
 
+        #sets the previous ball to the position of the game ball
         prev_ball_x =  game_ball.x_pos
         prev_ball_y = game_ball.y_pos
 
@@ -348,9 +357,10 @@ while running:
             if ball.x_pos + ball.radius >= WINDOW_WIDTH - 200:
                 ball.x_pos = WINDOW_WIDTH - 200 - ball.radius
                 ball.x_vel *= -1
-
+            #this resolves the hitboxes and collision of the balls
             resolve_ball_to_rect_collision(ball, prev_ball_x, prev_ball_y, player2Paddle.rect)
             resolve_ball_to_rect_collision(ball, prev_ball_x, prev_ball_y, player2Paddle.rect)
+            #loops through the brick and checks to see if the new spawned in balls collided with rectangles, and if so adds money
             for brick in bricks[:]:
                 extra_ball_rect = pygame.Rect(
                     ball.x_pos - ball.radius, ball.y_pos - ball.radius, ball.radius * 2, ball.radius * 2
@@ -386,6 +396,7 @@ while running:
         laser_cooldown_time = max(.1, base_cooldown- upgrades["laser_cooldown"]["level"] * .25)
         laser_dmg = 1 + upgrades["laser_damage"]["level"]
 
+        #handles the shooting of the laser
         shot1 = player1Paddle.inputs(dt)
         player1_laser_cooldown = max(0, player1_laser_cooldown-dt) # countdown
         if shot1 and player1_laser_cooldown == 0:
@@ -396,13 +407,15 @@ while running:
             )
             player1_laser_cooldown = laser_cooldown_time
 
-
+        #iterates through the laser array, and updates them, and despawns them when they go off the screen, and draws the lasers to the surface
         for laser in player1_lasers:
             laser.update(dt)
         player1_lasers = [laser for laser in player1_lasers if laser.rect.top < WINDOW_HEIGHT]
         for laser in player1_lasers:
             laser.draw(display_surface)
 
+
+        #draws player 1s paddle to the surface
         player1Paddle.draw(display_surface)
 
         # ball–paddle 1
@@ -420,6 +433,7 @@ while running:
         shot2 = player2Paddle.inputs(dt)
         player2_laser_cooldown = max(0, player2_laser_cooldown-dt) # countdown
 
+        #laser shooting implementation
         if shot2 and player2_laser_cooldown == 0:
             x2 = player2Paddle.rect.centerx - 2
             y2 = player2Paddle.rect.top - 12
@@ -427,7 +441,7 @@ while running:
                 Laser(x2, y2, length=4, width=12, color="red", speed=-600)
             )
             player2_laser_cooldown = laser_cooldown_time #cooldown, once it has been shot it has a one second cooldwon
-
+        #handles the updating of the lasers, the despawning and draws the lasers
         for laser in player2_lasers:
             laser.update(dt)
         player2_lasers = [laser for laser in player2_lasers
@@ -435,6 +449,8 @@ while running:
         for laser in player2_lasers:
             laser.draw(display_surface)
 
+
+        #draws player 2s paddle
         player2Paddle.draw(display_surface)
 
         # ball–paddle 2
@@ -472,9 +488,10 @@ while running:
                 resolve_ball_to_rect_collision(game_ball, prev_ball_x, prev_ball_y, brick.rect)
                 hit_bricks.append(brick)
 
+        #goes throught the hit_bricks array and makes them take damager
         for brick in hit_bricks:
             brick.take_damage(game_ball.power)
-            
+            #code for when a brick dies, removing it, adding money to the players
             print(brick.is_dead())
             if brick.is_dead():
                 bricks.remove(brick)
@@ -491,7 +508,7 @@ while running:
                     if brick.is_dead():
                         bricks.remove(brick)
                         players_money += 10
-                        player1_score += 1
+                        player1_score += 1 #if laser from player 1 collides with a brick then player 1 gets a point
                     break #one brick per laser
         player1_lasers = [l for l in player1_lasers if l not in lasers_to_remove]
         #laser collision section
@@ -509,11 +526,6 @@ while running:
                     break #one brick per laser
         player2_lasers = [l for l in player2_lasers if l not in lasers_to_remove]
 
-
-
-
-
-    
         #check end of level
         if not bricks:
             level_index += 1
@@ -522,8 +534,9 @@ while running:
             else:
                 build_level(level_index)
             
-
+    #displays all of this code every frame
     pygame.display.flip()
     dt = clock.tick(60) / 1000
 
+#quits pygame
 pygame.quit()
